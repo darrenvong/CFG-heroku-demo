7dd76b2638b0507b7e774933db932551c2127693
Darren Vong <d.vongy@gmail.com>
2017-03-14 03:36:39 +0000

    Finishing off the app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import re
 from flask import Flask, render_template
 from helpers.fun_tweet_scraper import *
 
@@ -11,8 +12,16 @@ def index():
 
 @app.route('/latest_pauline_tweets')
 def latest_tweets():
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0"}
     search_url = "https://twitter.com/search"
     params = {"q" : query_builder()}
+    tweet_objs = get_funny_tweets(search_url, params=params, headers=headers)
+
+    date_matcher = re.compile("\d+-\d+-\d+")
+    start_date, end_date = date_matcher.findall(params["q"])
+
+    return render_template("display_tweets.htm.j2", tweets=tweet_objs,
+                            start=start_date, end=end_date)
 
 if __name__ == '__main__':
     if 'PORT' in os.environ:
--- helpers/fun_tweet_scraper.py
+++ helpers/fun_tweet_scraper.py
@@ -9,17 +9,25 @@ def query_builder():
     return "from:paulienuh " + funny_tweet_periods[randrange(3)]
 
 def get_funny_tweets(url, params={}, headers={}):
+    """Retrieves a list of funny tweets in the following format:
+    {"tweet": "...", "time": "...", "images": ["...", "..."]} (images list may be empty)
+    """
     my_funny_req = requests.get(url, params=params, headers=headers)
-    parser = BeautifulSoup(my_funny_req.text, "html.parser")
+    parser = BeautifulSoup(my_funny_req.text, "lxml")
     full_tweets = parser.select(".tweet .content")
 
-    # Extract tweet text and time print to console
-    for tweet in full_tweets:
-        print tweet.select_one("._timestamp").get_text()
-        print tweet.select_one(".tweet-text").get_text()
+    # super compact way of returning a list of dictionaries using a construct known
+    # as list comprehension
+    return [{
+                "text": tweet.select_one(".tweet-text").get_text(),
+                "time": tweet.select_one("._timestamp").get_text(),
+                "images": [image["src"] for image in tweet.select(".AdaptiveMedia img")]
+            }
+            for tweet in full_tweets]
 
 if __name__ == '__main__':
     AGENT_NAME = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0"
     headers = {"User-Agent": AGENT_NAME}
     params = {"q" : "from:paulienuh since:2011-12-01 until:2011-12-31"}
-    get_funny_tweets("https://twitter.com/search", params=params, headers=headers)
+    tweet_objs = get_funny_tweets("https://twitter.com/search", params=params, headers=headers)
+    print tweet_objs
